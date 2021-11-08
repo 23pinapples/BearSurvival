5156695787699ec94ff724cc878cb7b94bbf9f58
Constance Li <consl414@gmail.com>
2021-11-08 16:56:26 -0600

    Initial Commit

---
+++ BasicARApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BasicARApp
+//
+//  Created by Haiyi Li on 11/8/21.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
